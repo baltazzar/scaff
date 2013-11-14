ebddf03d45bdb3d684e18390950158cd9da085c3
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2013-11-14 18:47:42 -0200

    Passing gruntfile away as option for use in tasks.

--- scaff.js
+++ scaff.js
@@ -6,7 +6,7 @@ var shell = require('shelljs'),
 	gruntfile = __dirname + '/' + process.cwd().replace(re, '_') + '.js',
 	folderPath = path.relative(__dirname, process.cwd()),
 	task = process.argv.slice(2)[0],
-	command = 'grunt --gruntfile ' + gruntfile + ' ' + task + ' --folder=' + folderPath;
+	command = 'grunt --gruntfile ' + gruntfile + ' ' + task + ' --gf=' + gruntfile + ' --folder=' + folderPath;
 
 // Copia o conteúdo do arquivo scaff.js do usuário no arquivo Gruntfile.js do Scaff
 shell.cp('-f', 'scaff.js', gruntfile);
